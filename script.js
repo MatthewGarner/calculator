c2de2130887e15536b80dde63f7172606dc3f794
Matthew Garner <1matthewgarner@gmail.com>
2021-04-11 17:19:51 +0100

    PADD: event listening for button presses and prettified

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Select dynamic content areas
+const calculatorDisplay = document.getElementById('calculator-output');
+const numberButtons = document.querySelectorAll('.number-input');
+const operatorButtons = document.querySelectorAll('.operator-input');
+
+//Basic functions for maths operations
 function add(a, b) {
     return a + b;
 }
@@ -14,6 +20,7 @@ function divide (a, b) {
     return a / b;
 }
 
+//General operator function
 function operate(operator, a, b) {
     switch (operator) {
         case "+":
@@ -28,3 +35,26 @@ function operate(operator, a, b) {
             return "ERROR";
     }
 }
+
+//Default page load
+    //TODO - Define default behaviour
+function loadDefault() {
+    console.log("test");
+}
+
+//Add event listeners for inputs
+numberButtons.forEach(element => element.addEventListener('click', e => console.log(e.target.textContent)));
+operatorButtons.forEach(element => element.addEventListener('click', e => console.log(e.target.textContent)));
+
+
+let displayNumber = "1";
+calculatorDisplay.textContent = displayNumber;
+
+/*
+Create the functions that populate the display when you click the number buttons… 
+you should be storing the ‘display value’ in a variable somewhere for use in the next step.
+*/
+
+//listen for button press
+//check which key was pressed and match to number
+//add number to screen display
